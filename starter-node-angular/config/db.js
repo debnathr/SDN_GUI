123a55d9f2058fd7f8109bd06e2817e3226d4b87
Rubi Debnath <rubidebnath@dhcp-10-180-27-103.dynamic.eduroam.mwn.de>
2018-07-07 22:15:21 +0200

    changes in collection anem

--- starter-node-angular/config/db.js
+++ starter-node-angular/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-	url : 'mongodb://sdn:sdnlab_123@18.219.185.25:27017/elasticon'
+	// url : 'mongodb://sdn:sdnlab_123@18.219.185.25:27017/elasticon'
+	url : 'mongodb://localhost:27017/elastiCon'
 }
--- starter-node-angular/controller/dataChange.js
+++ starter-node-angular/controller/dataChange.js
@@ -3,10 +3,10 @@
 module.exports = function (
       app,
       io,
-      tutorialspoint // Collection Name
+      controllers // Collection Name
 ) {
     // SET WATCH ON COLLECTION 
-    const changeStream = tutorialspoint.watch();  
+    const changeStream = controllers.watch();  
 
     // Socket Connection  
     io.on('connection', function (socket) {
@@ -16,7 +16,7 @@ module.exports = function (
         changeStream.on('change', function(change) {
             console.log('COLLECTION CHANGED');
 
-            tutorialspoint.find({}, (err, data) => {
+            controllers.find({}, (err, data) => {
                 if (err) throw err;
   
                 if (data) {
